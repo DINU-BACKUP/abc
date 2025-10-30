9fdc87b51d566d64d4db045a04e2754962cbcf0a
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2025-10-30 18:15:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-//SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=d216ed4e275c4683' : process.env.SESSION_ID, //botmakepaid sep9pcontac ( paid oct 27=rs.320)
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=8eb1b5dd886773a0' : process.env.SESSION_ID, //botmakepaid sep9pcontac ( paid oct 27=rs.320)
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
